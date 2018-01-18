7e2117a147a09ddd8d4f78e00f8cb6c5412a5e0d
AdrielFreud <gamerbra39@gmail.com>
2018-01-18 02:28:02 -0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -4,7 +4,7 @@ menu = r'''
 -----------------------------------------
 
   #  Desenvolvido por Adriel Freud!
-  #  Contato: usuariocargo2016@gmail.com 
+  #  Contato: businessc0rp2k17@gmail.com 
   #  FB: http://www.facebook.com/xrn401
   #   =>DebutySecTeamSecurity<=
 
